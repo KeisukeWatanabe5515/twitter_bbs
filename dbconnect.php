4a00da658bc467aab5f26b612380e76731e23f5f
hiroki7w <hiroki7w@gmail.com>
2020-05-22 10:52:44 +0000

    登録画面

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,9 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname=hirokei_db01;host=localhost;charset=utf8', 'user01', 'shusaku726');
+    $db = new PDO('mysql:dbname=hirokei_db01;host=127.0.0.1;charset=utf8', 'root', '');
 
 } catch(PDOException $e) {
     print('DB接続エラー:' . $e->getMessage());
 }
+
+
--- join/index.php
+++ join/index.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
+require('../dbconnect.php');
 
-if(!empty($_POST)) {
+if (!empty($_POST)) {
     // 名前の判定
     $name = str_replace(array(' ', '　'), '', $_POST['name']);
 
@@ -15,9 +16,7 @@ if(!empty($_POST)) {
     if ($email === "") {
         $error['email'] = 'blank';
     } 
-    else if (strpos($email, '@') === false) {
-            $error['email'] = 'format';
-    }
+ 
 
     // パスワードの判定
     $password = str_replace(array(' ', '　'), '', $_POST['password']);
@@ -43,11 +42,21 @@ if(!empty($_POST)) {
         }
     }
     
-    // ここにアカウントの重複チェックを記入
+    // アカウントの重複チェック
+    if (empty($error)) {
+       $member = $db->prepare('SELECT COUNT(*) AS cnt FROM users where email=?');
+
+       $member->execute(array($email));
+       $record = $member->fetch();
+       if ($record['cnt'] > 0) {
+           $error['email'] = 'duplicate';
+       }
+
+    }
 
     // 確認画面への以降処理
     if (empty($error)) {
-        if (!empty($_FILES)) {        
+        if (!empty($_FILES['image']['name'])) {        
             $ext = substr($fileName, -3);
             $image = date('YmdHis') . '.' . $ext;
 
@@ -61,81 +70,89 @@ if(!empty($_POST)) {
         $_SESSION['join']['image'] = $image;
 
         header('Location: check.php');
+        exit();
     }
 
+    
+
+
 }
+if ($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])) {
+    $_POST = $_SESSION['join'];
+    $name = str_replace(array(' ', '　'), '', $_POST['name']);
+    $email = str_replace(array(' ', '　'), '', $_POST['email']);
+}
+
 ?>
 
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>会員登録</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/application.css" />
+    <link rel="stylesheet" href="../css/join.css" />
 </head>
 <body>
+      <header>
+        <h1>会員登録</h1>
+    </header>
 <div class="container">
-<h1>会員登録</h1>
+  
 
 <p>次のフォームに必要事項を入力して下さい</p>
-
-<p class="error">
-    <ul>
             <?php
             if ($error['name'] === 'blank') : ?>
-                <li>ニックネームを入力してください</li>
+                <div class="error"> * ニックネームを入力して下さい</div>
             <?php endif; ?>
             <?php
             if ($error['email'] === 'blank') : ?>
-                <li>メールアドレスを入力して下さい</li>
-            <?php elseif ($error['name'] === 'format'): ?>
-                <li>メールアドレスの形式が間違っています</li>
+                <div class="error">* メールアドレスを入力して下さい</div>
+            <?php elseif ($error['email'] === 'duplicate'): ?>
+                <div class="error">* 指定したメールアドレスは、既に登録されています</div>
             <?php endif; ?>
             <?php
             if ($error['password'] === 'blank') : ?>
-                <li>パスワードを入力して下さい</li>
+                <div class="error">* パスワードを入力して下さい</div>
             <?php elseif ($error['password'] === 'length'): ?>
-                <li>パスワードは4文字以上20文字以内で入力して下さい</li>
+                <div class="error">* パスワードは4文字以上20文字以内で入力して下さい</div>
             <?php elseif ($error['password'] === 'mismatch'): ?>
-                <li>パスワードが一致していません</li>
+                <div class="error">* パスワードが一致していません</div>
             <?php endif; ?>
             <?php
             if ($error['image'] === 'type') : ?>
-                <li>画像ファイルは拡張子が「jpg」「gif」「png」のものを指定して下さい</li>
+                <div class="error">* 画像ファイルは拡張子が「jpg」「gif」「png」のものを指定して下さい</div>
             <?php endif; ?>
-    </ul>
-</p>
 
 <form action="" method="POST" enctype="multipart/form-data">
-    <dl>
-        <dt>ニックネーム <span class="required">必須</span></dt>
-        <dd>
-            <input type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($name)); ?>">
-        </dd>
-
-        <dt>メールアドレス <span class="required">必須</span></dt>
-        <dd>
-            <input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($email)); ?>">
-        </dd>
-
-        <dt>パスワード <span class="required">必須</span></dt>
-        <dd>
-            <input type="password" name="password" size="15" maxlength="20" value="">
-        </dd>
-
         
-        <dt>パスワード（確認）</dt>
-        <dd>
-            <input type="password" name="re_password" size="15" maxlength="20" value="">
+
+        <dl>
+        <dt><label for="name">ニックネーム <span class="badge badge-danger">必須</span></label> </dt>
+        <dd><input id="name" type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($name)); ?>"></dd>
+
+        <dt><label for="email">メールアドレス <span class="badge badge-danger">必須</span></label></dt>
+        <dd><input id="email" type="email" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($email)); ?>">
         </dd>
 
-        <dt>写真など</dt>
-        <dd>
-            <input type="file" name="image" size="35" value="test">
-			<?php if(!empty($error) && empty($error['image'])) : ?>
-			<p class="re_image">* 恐れ入りますが、画像をもう一度指定してください</p>
+        <dt><label for="password">パスワード <span class="badge badge-danger">必須</span></label></dt>
+        <dd><input id="password" type="password" name="password" size="15" maxlength="20" value=""></dt></dd>
+
+        <dt><label for="re_passsword">パスワード(確認)</label>
+            </dt>
+        <dd><input id=re_password type="password" name="re_password" size="15" maxlength="20" value=""></dd>
+
+        <dt><label for="image">写真など</label></dt>
+            <dd><input id="image" type="file" name="image" size="35" value="test">
+            <?php if(!empty($error) && !empty($_FILES['image']['name']) &&$error['image'] != "type") : ?>
+                <p class="error">* 恐れ入りますが、画像をもう一度指定してください</p>
             <?php endif; ?>
-        </dd>
-    </dl>
+            </dd>
+        </dl>
     <div>
         <input type="submit" value="入力内容を確認する">
     </div>
